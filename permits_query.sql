c2d8cc1684e430c7477ad3fede1992c26023eb13
soph <174645897+uncutnails@users.noreply.github.com>
2026-08-17 22:13:32 -0400

    Refactor SQL query to improve readability and consistency; update table names and column references.

--- permits_query.sql
+++ permits_query.sql
@@ -1,21 +1,19 @@
---tell PostgreSQL to expect Month/Day/Year format for this session
-SET datestyle = 'ISO, MDY';
-with get_median AS (
-	select percentile_cont(0.5) within group (order by NULLIF(TRIM("Construction Value"::text), '')::numeric) as construction_median_value -- percentile functions require ordered data so witihn gorup & order by goes together
-	from kw_building_permits
-),
-median_construction_calc as ( --subquery
-	select *, (select construction_median_value from get_median) as construction_median_value
-	from kw_building_permits
+WITH get_median AS (
+    SELECT 
+        PERCENTILE_CONT(0.5) WITHIN GROUP (
+            ORDER BY NULLIF(TRIM("CONSTRUCTION_VALUE"::text), '')::NUMERIC
+        ) AS construction_median_value 
+    FROM kitchener_buildings_permits
 )
-select *,
-	EXTRACT(month from "Application Date"::date) as "Application_Month",
-	COALESCE("Work Type", 'Unknown') as "Clean_Work_Type",
-	COALESCE("Final Date"::text, 'Unfinished') as "Clean_Final_Date",
-	COALESCE("Construction Value", construction_median_value) as "Clean_Construction_Value"
-from median_construction_calc 
-where (NULLIF(TRIM("Application Date"), '') IS NOT NULL)
-    AND (NULLIF(TRIM("Issue Date"), '') IS NOT NULL)
-    AND (NULLIF(TRIM("Issued By"), '') IS NOT NULL)
-
+select p.*,
+	EXTRACT(month from "APPLICATION_DATE") as "APPLICATION_MONTH",
+	COALESCE(p."WORK_TYPE", 'Unknown') as "CLEAN_WORK_TYPE",
+	COALESCE(p."FINAL_DATE"::text, 'Unfinished') as "CLEAN_FINAL_DATE",
+	COALESCE(p."CONSTRUCTION_VALUE", construction_median_value) as "CLEAN_CONSTRUCTION_VALUE"
+FROM kitchener_buildings_permits p
+CROSS JOIN get_median m
+where 
+	"APPLICATION_DATE" IS NOT NULL
+    AND "ISSUE_DATE" IS NOT NULL
+    AND "ISSUED_BY" IS NOT NULL
 
